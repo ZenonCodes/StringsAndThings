f231a95525df17d5bd25caba35e66a5f4c3ef074
Vera <vera@Zipcoders-MBP.fios-router.home>
2022-02-26 01:10:00 -0500

    first two exercises

--- src/main/java/io/zipcoder/StringsAndThings.java
+++ src/main/java/io/zipcoder/StringsAndThings.java
@@ -1,6 +1,8 @@
 package io.zipcoder;
 
 
+import java.util.Locale;
+
 /**
  * @author tariq
  */
@@ -14,8 +16,24 @@ public class StringsAndThings {
      *           countYZ("day fez"); // Should return 2
      *           countYZ("day fyyyz"); // Should return 2
      */
+
+
     public Integer countYZ(String input){
-        return null;
+        int count = 0;
+        // first convert string to lower case so that result is not case-sensitive
+        input = input.toLowerCase();
+        // next separate all words in string using space as delimiter
+        String [] stringList = input.split(" ");
+        // loop through our string array and get rid of any extra whitespaces in each string (in case there was a double space)
+        for (int i = 0; i < stringList.length; i++){
+            stringList[i] = stringList[i].trim();
+            // check to see if the last letter in the string is "z" or "y" and add to count if true
+            if (stringList[i].endsWith("z") || stringList[i].endsWith("y")){
+                count ++;
+            }
+        }
+
+        return count;
     }
 
     /**
@@ -27,8 +45,31 @@ public class StringsAndThings {
      *           removeString("Hello there", "e") //  Should return "Hllo thr"
      *           removeString("Hello there", "x") // Should return "Hello there"
      */
+
+    /** References: https://www.geeksforgeeks.org/stringbuilder-replace-in-java-with-examples/
+     *             https://stackoverflow.com/questions/7775364/how-can-i-remove-a-substring-from-a-given-string
+     */
     public String removeString(String base, String remove){
-        return null;
+        int substringLength = remove.length();
+        String baseLower = base.toLowerCase();
+        String removeLower = remove.toLowerCase();
+        boolean containsSubstring =  baseLower.contains(removeLower);
+        // if base does not contain remove then return base
+        if (!containsSubstring){
+            return base;
+        }
+
+        // Find the beginning of substring remove in base string and remove it until no more
+        // instance of substring occur
+        StringBuilder editBase = new StringBuilder(base);
+        while ((base.toLowerCase()).contains(removeLower)){
+            int subStart = (base.toLowerCase()).indexOf(removeLower);
+            int subEnd = subStart + substringLength;
+            editBase = editBase.replace(subStart, subEnd, "");
+            base = editBase.toString();
+
+        }
+        return base;
     }
 
     /**
